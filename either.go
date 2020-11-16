a4c9280e3a64b72276b5f606429f120dede23b60
Lukasz Szpakowski <luckboy@vp.pl>
2020-11-16 12:01:34 +0100

    First commit.

---
+++ either.go
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2020 Łukasz Szpakowski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package gofun
+
+type Either struct {
+    isRight bool
+    x interface{}
+}
+
+func Left(x interface{}) *Either {
+    return &Either { isRight: false, x: x }
+}
+
+func Right(x interface{}) *Either {
+    return &Either { isRight: true, x: x }
+}
+
+func (o *Either) IsRight() bool {
+    return o.isRight
+}
+
+func (e *Either) GetLeft() interface{} {
+    if e.isRight {
+        return nil
+    } else {
+        return e.x
+    }
+}
+
+func (e *Either) GetRight() interface{} {
+    if e.isRight {
+        return e.x
+    } else {
+        return nil
+    }
+}
+
+func (e *Either) GetLeftOrElse(x interface{}) interface{} {
+    if e.isRight {
+        return x
+    } else {
+        return e.x
+    }
+}
+
+func (e *Either) LeftOrElse(e2 *Either) interface{} {
+    if e.isRight {
+        return e2
+    } else {
+        return e
+    }
+}
+
+func (e *Either) GetRightOrElse(x interface{}) interface{} {
+    if e.isRight {
+        return e.x
+    } else {
+        return x
+    }
+}
+
+func (e *Either) RightOrElse(e2 *Either) interface{} {
+    if e.isRight {
+        return e
+    } else {
+        return e2
+    }
+}
---
+++ functor.go
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 Łukasz Szpakowski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package gofun
+
+type Functor interface {
+    Map(func(interface{}) interface{}) Functor
+}
+
+func (xs *Option) Map(f func(interface{}) interface{}) Functor {
+    if xs.IsSome() {
+        return Some(f(xs.Get()))
+    } else {
+        return None()
+    }
+}
+
+func (xs *Either) Map(f func(interface{}) interface{}) Functor {
+    if xs.IsRight() {
+        return Right(f(xs.GetRight()))
+    } else {
+        return Left(xs.GetLeft())
+    }
+}
+
+func (xs InterfaceSlice) Map(f func(interface{}) interface{}) Functor {
+    ys := make([]interface{}, 0, len(xs))
+    for _, x := range xs {
+        ys = append(ys, f(x))
+    }
+    return InterfaceSlice(ys)
+}
